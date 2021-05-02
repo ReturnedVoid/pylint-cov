b1cea8f31f223624a564bf5041fe1f65e708f1e1
Andrii Nechaev <andreynech@aswsyst.cz>
2021-05-02 08:50:53 +0300

    v.0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylint-cov',
-    version='0.0.1',
+    version='0.0.2',
     author='Andrey Nechaev',
     author_email='andrewnech@gmail.com',
     packages=find_packages(),
